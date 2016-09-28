f3e6bc241f277a350a95eb33115810a8d1658bee
thselvam <thiyagarajan.selvam@outlook.com>
2016-09-28 14:01:01 -0700

    updated with comments

--- left_shift_array.c
+++ left_shift_array.c
@@ -15,13 +15,20 @@ int main(){
        scanf("%d",&a[a_i]);
     }
     
+    /* Allocate a new array to hold the new shifted value */    
     int *b = malloc(sizeof(int) * n);
-        
+    
+    /*
+     * copy over the existing array contents to new array:
+     * from kth position of existing to the new array's 0th index
+     * This can be acheived by modulo of the array index as below
+     */
     for(int a_i=k, b_i=0; b_i < n; a_i++, b_i++)
     {
         b[b_i] = a[a_i%n];
     }
     
+    /* Just print it */
     for(int b_i=0; b_i < n;b_i++)
     {
         printf("%d ", b[b_i]);        
